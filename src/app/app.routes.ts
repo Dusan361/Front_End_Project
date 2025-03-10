da02b178ffa3ffd9c6363412c00113712fb10f45
Svetlana Shmeleva <svetlana.shmeleva.1@gmail.com>
2025-03-10 17:45:57 +0100

    Home and Review

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -4,12 +4,14 @@ import { CoursesComponent } from './courses/courses.component';
 import { AboutComponent } from './about/about.component';
 import { TrainersComponent } from './trainers/trainers.component';
 import { VideosComponent } from './videos/videos.component';
+import { ReviewComponent } from './review/review.component';
 
 export const routes: Routes = [
 
-    { path: " ", component: HomeComponent },
+    { path: "", component: HomeComponent },
     { path: "courses", component: CoursesComponent },
     { path: "about", component: AboutComponent },
     { path: "trainers", component: TrainersComponent },
-    { path: "videos", component: VideosComponent }
+    { path: "videos", component: VideosComponent },
+    { path: "review", component: ReviewComponent }
 ];
