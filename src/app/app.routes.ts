87696e20d6fd4539fdc97f5b8c195188be4b1f59
Miikii97 <milenovikjmiki@gmail.com>
2025-03-10 14:13:45 +0100

    Routes Added

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,13 @@
 import { Routes } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { CoursesComponent } from './courses/courses.component';
+import { AboutComponent } from './about/about.component';
+import { TrainersComponent } from './trainers/trainers.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+
+    { path: " ", component: HomeComponent },
+    { path: "about", component: CoursesComponent },
+    { path: "about", component: AboutComponent },
+    { path: "trainers", component: TrainersComponent }
+];
