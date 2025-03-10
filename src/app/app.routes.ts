687576f116895f527551706aa9e94adc0ec81b31
Dusan Krtinic <dule147@gmail.com>
2025-03-10 14:17:13 +0100

    Videoplayer

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,9 @@
 import { Routes } from '@angular/router';
+import { VideosComponent } from './videos/videos.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+   {
+    path: "videos",
+    component: VideosComponent
+   }
+];
