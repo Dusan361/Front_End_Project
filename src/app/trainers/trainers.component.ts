16979bae8e6a45a28c9e411817329dcec4aceb0c
Barisic Luka <barisicluka33@gmail.com>
2025-03-10 14:21:35 +0100

    trainers

--- src/app/trainers/trainers.component.ts
+++ src/app/trainers/trainers.component.ts
@@ -1,8 +1,9 @@
 import { Component } from '@angular/core';
+import { RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-trainers',
-  imports: [],
+  imports: [RouterModule],
   templateUrl: './trainers.component.html',
   styleUrl: './trainers.component.css'
 })
