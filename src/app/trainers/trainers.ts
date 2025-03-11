87ae392ccfa11c8c020f835afc0d53c0204a1aeb
Miikii97 <milenovikjmiki@gmail.com>
2025-03-11 11:36:25 +0100

    Media for trainers

--- src/app/trainers/trainers.ts
+++ src/app/trainers/trainers.ts
@@ -1,28 +1,28 @@
 import { Trainers } from "./trainers.model";
 
 export const TRAINERS : Trainers [] = [
-    {
-    name: "Nikola",
-    age: 21,
-    img:"NIKOLA.jpg",
-    description: "Nikola is an enthusiastic and motivated new fitness trainer, eager to help clients reach their health and fitness goals. With a focus on strength, endurance, and overall wellness, they create personalized workout plans and provide consistent support. Nikola is dedicated to learning and growing in their fitness journey."
-    },
+    // {
+    // name: "Nikola",
+    // age: 21,
+    // img:"NIKOLA.jpg",
+    // description: "Nikola is an enthusiastic and motivated new fitness trainer, eager to help clients reach their health and fitness goals. With a focus on strength, endurance, and overall wellness, they create personalized workout plans and provide consistent support. Nikola is dedicated to learning and growing in their fitness journey."
+    // },
     {  
     name: "Hassan",
     age: 23,
     img:"hassan.jpg",
     description: "Hassan is an exceptional, certified fitness coach with a true passion for helping clients reach their fitness goals. With personalized workout plans, unwavering motivation, and expert guidance, they specialize in strength, endurance, and overall wellness. Hassan inspires confidence, pushes limits, and transforms lives through dedication and expertise.",
     },
-    { 
-    name: "Dwayne",
-    age: 25,    
-    img:"dwayne.jpg",
-    description: "Dwayne is a certified fitness expert dedicated to helping clients achieve their goals with personalized workout plans, motivation, and expert guidance. Focused on strength, endurance, and overall wellness.",
-    },
     {
     name: "Marco",
     age: 27,
     img:"petar.jpg",
     description: "Marco is a certified fitness coach passionate about helping clients reach their fitness goals through tailored workouts, motivation, and expert guidance, focusing on strength, endurance, and overall health.",
+    },
+    { 
+        name: "Dwayne",
+        age: 25,    
+        img:"dwayne.jpg",
+        description: "Dwayne is a certified fitness expert dedicated to helping clients achieve their goals with personalized workout plans, motivation, and expert guidance. Focused on strength, endurance, and overall wellness.",
     },
 ];
