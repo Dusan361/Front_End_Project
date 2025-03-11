c30b1aa1aa3cf097ea9cf553a9d09231a381c6f8
Miikii97 <milenovikjmiki@gmail.com>
2025-03-11 22:55:21 +0100

    transitions

--- src/app/trainers/trainers.component.ts
+++ src/app/trainers/trainers.component.ts
@@ -2,12 +2,20 @@ import { Component, EventEmitter, OnInit } from '@angular/core';
 import { RouterModule } from '@angular/router';
 import { Trainers } from './trainers.model';
 import { TRAINERS } from './trainers';
+import { animate, group, state, style, transition, trigger } from '@angular/animations';
 
 @Component({
   selector: 'app-trainers',
   imports: [RouterModule],
   templateUrl: './trainers.component.html',
-  styleUrl: './trainers.component.css'
+  styleUrl: './trainers.component.css',
+  animations: [
+    trigger('expand', [
+      // state('in', style({height: '0px'})),
+      transition(':enter', [style({height: '0px'}), animate('200ms', style({height: '200px'}))]),
+      transition(':leave', [animate('200ms', style({height: '0px'}))]),
+    ]),
+  ],
 })
 export class TrainersComponent implements OnInit{
    emiter = new EventEmitter<number | null>
