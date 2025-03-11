dbef469aaf1d8416c0ec6c47f8abb2369829434d
Miikii97 <milenovikjmiki@gmail.com>
2025-03-11 14:39:52 +0100

    Router added to courses

--- src/app/courses/courses.component.ts
+++ src/app/courses/courses.component.ts
@@ -1,8 +1,9 @@
 import { Component } from '@angular/core';
+import { RouterLink, RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-courses',
-  imports: [],
+  imports: [RouterLink, RouterModule],
   templateUrl: './courses.component.html',
   styleUrl: './courses.component.css'
 })
