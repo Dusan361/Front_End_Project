96088152348032b6a2d05e19576809f9d2cd8c2a
Barisic Luka <barisicluka33@gmail.com>
2025-03-11 10:41:41 +0100

    trainers

--- src/app/trainers/trainers.ts
+++ src/app/trainers/trainers.ts
@@ -5,24 +5,24 @@ export const TRAINERS : Trainers [] = [
     name: "Nikola",
     age: 21,
     img:"NIKOLA.jpg",
-    description: "Nikola is a great trainer"
+    description: "Nikola is an enthusiastic and motivated new fitness trainer, eager to help clients reach their health and fitness goals. With a focus on strength, endurance, and overall wellness, they create personalized workout plans and provide consistent support. Nikola is dedicated to learning and growing in their fitness journey."
     },
     {  
     name: "Hassan",
     age: 23,
-    img:"",
-    description: "Hasan is a great trainer",
+    img:"hassan.jpg",
+    description: "Hassan is an exceptional, certified fitness coach with a true passion for helping clients reach their fitness goals. With personalized workout plans, unwavering motivation, and expert guidance, they specialize in strength, endurance, and overall wellness. Hassan inspires confidence, pushes limits, and transforms lives through dedication and expertise.",
     },
     { 
-    name: "Ivan",
+    name: "Dwayne",
     age: 25,    
-    img:"",
-    description: "Ivan is a great trainer",
+    img:"dwayne.jpg",
+    description: "Dwayne is a certified fitness expert dedicated to helping clients achieve their goals with personalized workout plans, motivation, and expert guidance. Focused on strength, endurance, and overall wellness.",
     },
     {
-    name: "Petar",
+    name: "Marco",
     age: 27,
-    img:"",
-    description: "Petar is a great trainer",
+    img:"petar.jpg",
+    description: "Marco is a certified fitness coach passionate about helping clients reach their fitness goals through tailored workouts, motivation, and expert guidance, focusing on strength, endurance, and overall health.",
     },
 ];
